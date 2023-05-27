4dfd06517d1fbd54b0cbb479affd149b582d3022
OlehPetryshyn <olegpetryshen@gmail.com>
2023-05-27 19:53:57 +0300

    My unit test

---
+++ src/test/java/people/VariablesWoman.java
@@ -0,0 +1,11 @@
+package people;
+
+import org.people.Man;
+
+public class VariablesWoman {
+    VariablesMan variablesMan = new VariablesMan();
+    String firstNameWoman = "Olga";
+    String lastNameWoman = "Fraimut";
+    int ageWoman = 60;
+    String partner = variablesMan.lastNameMan;
+}
