76581d87502b171448454a0a065f4333448fbe94
OlehPetryshyn <olegpetryshen@gmail.com>
2023-05-30 14:23:27 +0300

    My updated unit test

--- src/test/java/people/TestMan.java
+++ src/test/java/people/TestMan.java
@@ -4,28 +4,61 @@ package people;
 import org.people.Man;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public  class TestMan extends PersonTest {
-    VariablesMan variablesMan = new VariablesMan();
-    @Override
+import static java.lang.String.format;
+import static org.testng.Assert.assertTrue;
+
+public  class TestMan  {
+
+    private Man man;
+
     @BeforeTest
-    public void creatingPeople() {
-        Man man = new Man(variablesMan.firstNameMan, variablesMan.lastNameMan, variablesMan.ageMan);
+   @Parameters({
+        "firstNameMan",
+        "lastNameMan",
+        "ageMan"
+})
+    public void creatingMan(String firstNameMan , String lasNameMan , int ageMan) {
+         man = new Man(firstNameMan, lasNameMan, ageMan);
     }
 
-    @Override
+
     @Test
     public void testPeopleHaveRetired() {
-            Assert.assertEquals(variablesMan.ageMan, 65,"This man have retired");
+        Assert.assertTrue(man.isRetired(), format("Man %s with age %d should not be retired", man.getFirstName() +" "+ man.getLastName(), man.getAge()));
+    }
+    @Test
+    public void testGetFirstName(){
+    Assert.assertEquals(man.getFirstName(),"John", format("This man have incorrect firstname. " + "The firstname should be " + man.getFirstName()));
+}
+    @Test
+    public void testGetLastName(){
+        Assert.assertEquals(man.getLastName(), "Jonson", format("This man have incorrect lastname. " + "The lastname should be " + man.getLastName()));
+    }
+@Test
+    public void  testGetAge(){
+        Assert.assertEquals(man.getAge(),65, "This man have incorrect age");
+}
+    @Test
+    public void testSetFirstName(){
+        man.setFirstName("Tom");
+        Assert.assertEquals(man.getFirstName(),"Tom", format("This Woman have incorrect firstname. " + "The firstname should be " + man.getFirstName()));
+    }
+    @Test
+    public void testSetLastName(){
+        man.setLastName("Tom") ;
+        Assert.assertEquals(man.getLastName(), "Tom", format("This Woman have incorrect lastname. " + "The lastname should be " + man.getLastName()));
+    }
+    @Test
+    public void  testSetAge() {
+        man.setAge(60);
+        Assert.assertEquals(man.getAge(), 60, "This Woman have incorrect age");
+
     }
 
-    @Override
-    public void testRegisterPartnership() {
-    }
-   @Test
-    @Override
-    public void testDeregisterPartnership() {
-        Assert.assertEquals(man, man ,"Woman deregister partnership");
-    }
+
+
+
 }
--- src/test/java/people/TestWoman.java
+++ src/test/java/people/TestWoman.java
@@ -1,32 +1,125 @@
 package people;
+import org.people.Man;
+import org.people.Person;
 import org.people.Woman;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import static java.lang.String.format;
 
-public class TestWoman extends  PersonTest{
-    VariablesWoman variablesWoman = new VariablesWoman();
-    VariablesMan variablesMan = new VariablesMan();
-    @Override
+
+public class TestWoman {
+
+    private static class TestPerson extends Person {
+        public TestPerson(String firstName, String lastName, int age) {
+            super(firstName, lastName, age);
+        }
+
+        @Override
+        public boolean isRetired() {
+            return false;
+        }
+
+        @Override
+        public void registerPartnership(Person partner) {
+
+        }
+
+        @Override
+        public void deregisterPartnership(boolean revertLastName) {
+
+        }
+
+    }
+
+
+private  Woman woman;
+
     @BeforeTest
-    public void creatingPeople() {
-        Woman woman = new Woman(variablesWoman.firstNameWoman, variablesWoman.lastNameWoman, variablesWoman.ageWoman);
+    @Parameters({
+            "firstNameWoman",
+            "lastNameWoman",
+            "ageWoman",
+
+    })
+    public void creatingWoman(String firstNameWoman, String lastNameWoman, int ageWoman) {
+         woman = new Woman(firstNameWoman, lastNameWoman, ageWoman);
     }
 @Test
-    @Override
+
     public void testPeopleHaveRetired() {
-    Assert.assertEquals(variablesWoman.ageWoman, 60,"This man have retired");
+
+    Assert.assertTrue(woman.isRetired(), format("Woman %s with age %d should not be retired", woman.getLastName() + " " + woman.getLastName(), woman.getAge()));
 
     }
-@Test
-    @Override
-    public void testRegisterPartnership() {
-        Assert.assertEquals(variablesWoman.partner,variablesMan.lastNameMan,"This woman has partner");
+
+
+    @Test
+    @Parameters({
+            "firstNameMan",
+            "lastNameMan",
+            "ageMan"
+    })
+    public void testRegisterPartnership(String firstNameMan , String lasNameMan , int ageMan) {
+        woman.setLastName(woman.getLastName());
+        woman.setFirstName(woman.getFirstName());
+
+        TestPerson partner = new TestPerson(firstNameMan, lasNameMan, ageMan);
+        partner.setLastName("Smith");
+        partner.setFirstName("John");
+
+        woman.registerPartnership(partner);
+
+        Assert.assertEquals(partner, woman.getPartner());
+        Assert.assertEquals(partner.getLastName(), woman.getLastName());
+        Assert.assertEquals("Doe", woman.getPreviousLastName());
+    }
+    @Test
+    @Parameters({
+            "firstNameMan",
+            "lastNameMan",
+            "ageMan"
+    })
+    public void testDeregisterPartnership_revertLastName(String firstNameMan , String lasNameMan , int ageMan) {
+
+        Man man = new Man(firstNameMan, lasNameMan ,ageMan);
+        woman.registerPartnership(man);
+
+        woman.deregisterPartnership(true);
+
+        Assert.assertNull(woman.getPartner());
+        Assert.assertEquals("Doe", woman.getLastName());
+        Assert.assertEquals("Jonson", woman.getPreviousLastName());
+    }
+    @Test
+    public void testGetFirstName(){
+        Assert.assertEquals(woman.getFirstName(),"Jane", format("This Woman have incorrect firstname. " + "The firstname should be " + woman.getFirstName()));
+    }
+    @Test
+    public void testGetLastName(){
+        Assert.assertEquals(woman.getLastName(), "Doe", format("This Woman have incorrect lastname. " + "The lastname should be " + woman.getLastName()));
+    }
+    @Test
+    public void  testGetAge() {
+        Assert.assertEquals(woman.getAge(), 60, "This Woman have incorrect age");
 
     }
-@Test
-    @Override
-    public void testDeregisterPartnership() {
-        Assert.assertEquals(woman,woman,"Woman deregister partnership");
+
+    @Test
+    public void testSetFirstName(){
+        woman.setFirstName("Loly");
+        Assert.assertEquals(woman.getFirstName(),"Loly", format("This Woman have incorrect firstname. " + "The firstname should be " + woman.getFirstName()));
+    }
+    @Test
+    public void testSetLastName(){
+        woman.setLastName("Loly") ;
+        Assert.assertEquals(woman.getLastName(), "Loly", format("This Woman have incorrect lastname. " + "The lastname should be " + woman.getLastName()));
+    }
+    @Test
+    public void  testSetAge() {
+        woman.setAge(60);
+        Assert.assertEquals(woman.getAge(), 60, "This Woman have incorrect age");
+
     }
 }
